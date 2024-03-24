b24213047c7a3a78dafd9b5fc26ac2eb963d0b20
RiyadAhsan4516 <riyad.ahsan4516@gmail.com>
2024-03-24 14:27:19 +0600

    - reformatted folder structure - edited the Readme.md file

--- dist/src/helpers/errorChecker.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.methodTypeCheck = void 0;
-const boom_1 = require("@hapi/boom");
-function methodTypeCheck(req_type, expected_type) {
-    if (req_type !== expected_type)
-        throw (0, boom_1.methodNotAllowed)(`${req_type} method not allowed on this route`);
-}
-exports.methodTypeCheck = methodTypeCheck;
