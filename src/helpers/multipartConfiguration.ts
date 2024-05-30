633b26c68207905a178728a7871c9ab8f4f20c9c
RiyadAhsan4516 <riyad.ahsan4516@gmail.com>
2024-05-30 19:04:26 +0600

    - changed static file serving process

---
+++ src/helpers/multipartConfiguration.ts
@@ -0,0 +1,14 @@
+import {RouteOptionsPayload} from "@hapi/hapi";
+
+export function multipartConfig (size : number, timeout: number) : RouteOptionsPayload {
+    return {
+        allow: "multipart/form-data",
+        parse: true,
+        multipart: {
+            output: "file"
+        },
+        maxBytes: 1000 * 1000 * size, // In MB
+        timeout,
+        uploads: 'src/public/tmp',
+    }
+}
